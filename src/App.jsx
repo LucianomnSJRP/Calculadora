efd24c9867a66face3e4d5e48dd0052e4e1bdddd
luciano mendonca <luciano@proansi.com.br>
2023-10-27 17:59:36 -0300

    Ajuste Geral

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
 
   function mostrar() {
 
-    var teste = document.getElementById('id').value;
+    var teste = document.getElementById('edtlitros').value;
     var n2 = document.getElementById('limpar2').value;
 
-    var soma = ((parseInt(teste) + parseInt(n2)));
+    var soma = ((parseInt(n2) / parseInt(teste)));
 
     alert(soma);
 
@@ -28,6 +28,8 @@ function App() {
     const limpar2 = document.querySelector('#limpar2');
     limpar2.value = '';
 
+    const edtlitros = document.querySelector('#edtlitros');
+    edtlitros.value = '';
 
   }
 
@@ -42,13 +44,25 @@ function App() {
         <p className='index'>Calcular combustivel</p>
         <div className='conteudo'>
 
-          <label for='' id='txt1'> N1:</label>
+          <label for='' id='txt1'> Valor Combustivel:R$ </label>
+       
           <input className='edt' id='id'></input>
-
-          <label for='' id='txt2'> N2:</label>
+          <label for='' id='txt2'> Km Rodados:</label>
           <input className='edt2' id='limpar2'></input>
 
         </div>
+        <div className='topo2'>
+
+        <label for='' id='txtLitros'> Qtd Litros </label>
+        <input className='edtLitros' id='edtlitros'></input>
+
+        </div>
+      
+        
+        
+        
+        
+         <div/>
 
         <button className='btn-start' type='submit' onClick={mostrar}>Calcular</button>
 
